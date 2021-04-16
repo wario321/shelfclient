16d4f36d7b0c9113b3632d5c363344b7b6142df2
Dhannawat Sindhuseka <dhannawatsindhuseka@Dhannawats-MacBook-Pro.local>
2021-04-16 20:27:15 +0700

    shelf client

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://127.0.0.1:5000'
+    baseURL: 'https://shelflabelapi.herokuapp.com'
 })
 
 //const deploy = https://shelflabelapi.herokuapp.com
